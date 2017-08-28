ce7bebe043c8d65e341b75c155d8f15509341a38
KimCris <31276788+KimCris@users.noreply.github.com>
2017-08-28 18:13:54 +0100

    Update One.hs

--- src/One.hs
+++ src/One.hs
@@ -5,12 +5,51 @@ import qualified Data.Graph		   as  Gr
 import qualified Control.Monad     as  Cm
 
 
+data Graph t a = Empty
+               | Vertex a
+               | Overlay String (Graph t a) (Graph t a)
+               | Connect String String (Graph t a) (Graph t a)
+               | Subgraph String (Graph t a)
+               deriving (Show, Read, Eq)
 
-data Graph a = Empty
-             | Vertex a
-             | Overlay [a] [a]
-             | Connect [a] [a]
-             deriving (Show, Read, Eq)
+
+
+-- Visualisation of Graph as strings 
+
+
+visualise :: Graph String Int -> String
+visualise Empty         		= "\n"
+visualise (Vertex i)    		= show i
+visualise (Overlay n1 x y) 		= n1 ++ "[(" ++ visualise x ++ ")" ++ "," ++  "(" ++ visualise y ++ ")]"
+visualise (Connect n1 n2 x y) 	= visualiseAsCluster n1 x ++ visualiseAsCluster n2 y ++ n1 ++ " -> " ++ n2 ++ "\n"
+visualise (Subgraph n x)  		= visualiseAsCluster n x
+visualiseAsCluster :: String -> Graph String Int -> String
+visualiseAsCluster n g = "cluster " ++ n ++ " {\n" ++ visualise g ++ "}\n"
+
+
+-- element to display the graph as a list  
+type Vertex  = [Int]
+type Edge    = (Vertex, Vertex)
+type Graphs  = [Edge] 
+
+
+
+connect :: a -> a -> (a, a)
+connect x1 x2 = (x1,x2)
+
+overlay :: a -> a -> Maybe ([a],[a])
+overlay x1 x2 = Just ([x1],[x2])
+
+showEdges :: (Ord a) => [a] -> [a]  
+showEdges [] = []  
+showEdges (x:xs) =  (x:xs)
+
+showVertices :: (Ord a) => [a] -> [a]  
+showVertices [] = []  
+showVertices (x:xs) =   
+    let smaller = showVertices [a | a <- xs, a < x]  
+        bigger = showVertices [a | a <- xs, a > x]  
+    in  smaller ++ [x] ++ bigger
              
 type Vertex  = [Int]
 type Edge    = (Vertex, Vertex)
@@ -37,10 +76,8 @@ showVertices (x:xs) =
     
     
     
-    
-    
-    
--- Dot format 
+      
+-- Dot format, get list from previous sections and display as a dot format  
  
 graphOpen   = "digragrap " 
 graphClose  = "\n}"
@@ -57,38 +94,3 @@ nodeAtributes x1 x2  = x1 ++ " [" ++ x2 ++ "]"
 dotEdge :: String -> String -> String
 dotEdge x1 x2 = x1 ++ "->" ++ x2
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---main = interact dotNode 
-
---dotEdges ::[(a,b)] -> string
---dotEdges []  =  []
---dotEdges (xs) =  "alo" ++ (x,y)
---			  where y = head xs
-
-
--- later 
-
-
-
-
-{--
-visualise :: Graph String -> String
-visualise Empty         = empty 
---}
