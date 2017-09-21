2c4fc89550f64cefbb99f8aae83cdf06ff3d62fc
KimCris <31276788+KimCris@users.noreply.github.com>
2017-09-21 18:15:41 +0100

    Update One.hs

--- src/One.hs
+++ src/One.hs
@@ -1,85 +1,69 @@
 module DotSintax
 (
-
-dotSintax
-,visualise
-,visualiseCluster
-,dotDirect
-,dotNodes
-,dotEdges
-,main
-
+dotGraph
+,dotSintax
+,cluster
+,dotOpen
+,dotClose
+,dotSintax
 )where
 
 import qualified Data.Graph as Gr
 import  Data.Char
-import Control.Monad
-import System.IO
+import Control.Applicative
 
 
-
-data Graph t a = Empty
-               | Vertex a
-               | Overlay (Graph t a) (Graph t a)
-               | Connect String String (Graph t a) (Graph t a)
-               | Subgraph String (Graph t a)
+data Graph a = Empty
+              | Vertex a
+              | Overlay (Graph a) (Graph a)
+              | Connect (Graph a) (Graph a)
+              | Subgraph (Graph a)
                deriving (Show, Read, Eq)
-               
--- Approach to see Graph data in dot Sintax              
-               
-dotSintax :: String -> String -> Graph String Int -> String
-dotSintax x1 x2 (Empty)     = if x1 == "direct" 
-							then "Diagraph " ++ x2 ++ " { \n } " 
-							else  "graph " ++ x2 ++ " { \n } " 
-							
-dotSintax x1 x2 (Vertex x)  = if x1 == "direct" 
-							then "Diagraph " ++ x2 ++ " {" ++ show x ++ "\n } "
-							else  "graph " ++ x2 ++ " {" ++ show x ++ " \n } "  
+	       
+	       
+	       
+	       
+-- Num istances for the Graph a 
 
-dotSintax x1 x2 (Overlay n1 n2 )  = if x1 == "direct" 
-							then "Diagraph " ++ x2 ++ " {" ++ visualise n1 ++ visualise n2 ++ "\n } "
-							else  "graph " ++ x2 ++ " {" ++ visualise n1 ++ visualise n2 ++ "\n } "
+instance Num a => Num (Graph a) where
+    fromInteger = Vertex . fromInteger
+    (+)         = Overlay
+    (*)         = Connect
+    signum      = const Empty
+    abs         = undefined
+    negate      = Subgraph
+               
+-- Approach to see Graph data in dot Sintax   
+
+dotGraph :: Show a => Graph a -> String
+
+dotGraph (Empty)     =  " \n " 
+													
+dotGraph (Vertex x)  = show x 
+
+dotGraph  (Overlay n1 n2 )  =  dotGraph n1 ++ dotGraph n2
+														
+dotGraph (Connect n1 n2 )  = cluster n1 ++ cluster n2 
 							
-dotSintax x1 x2 (Connect x3 x4 n1 n2 )  = if x1 == "direct" 
-							then "Diagraph " ++ x2 ++ " {" ++ visualiseCluster x3 n1 ++ visualiseCluster x4 n2 ++ "\n } "
-							else  "graph " ++ x2 ++ " {" ++ visualiseCluster x3 n1 ++ visualiseCluster x4 n2 ++ "\n } "
-							
-dotSintax x1 x2 (Subgraph x3 n1 )  = if x1 == "direct" 
-							then "Diagraph " ++ x2 ++ " {" ++ visualiseCluster x3 n1 ++ "\n } "
-							else  "graph " ++ x2 ++ " {" ++ visualiseCluster x3 n1 ++  "\n } "						
+dotGraph  (Subgraph n1 )  = cluster n1 
+
+cluster :: Show a => Graph a -> String 
+cluster n = "Cluster {" ++ show n ++ "\n } "
+
 
 
 --Define Visualise 
-visualise :: Graph String Int -> String
-visualise Empty = error "nothing to visualise"
-visualise (Vertex x) = show x 
- 
 
-visualiseCluster :: String  -> Graph String Int -> String 
-visualiseCluster x n   = "Diagraph " ++ show x ++ " {" ++ visualise n ++ "\n } "
+dotOpen :: String
+dotOpen = " Diagraph {\n"
+
+dotClose :: String
+dotClose = "}\n"
+
+dotSintax :: Show a => Graph a -> String 
+dotSintax n = dotOpen ++ dotGraph n ++ dotClose
+               
 							
 							
--- Approach to produce dotfile using manads/IO
-
-
-dotDirect  :: String -> String
-dotDirect  = show 
- 
-dotNodes    :: String -> String
-dotNodes    = show
-
-dotEdges    ::Show a => (a,a) -> String
-dotEdges  (a,b) = show a ++ "->" ++ show b
-
-main  = do 
-	line <- getLine
-	line1 <- getLine
-	if null line 
-		then return ()
-		else do putStrLn $ dotDirect line ++ "{" 
-	
-	if null line1 
-		then return ()
-		else do putStrLn ( "node [" ++ dotNodes  line1 ++ "]")
 		
 
